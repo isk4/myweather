7433cce3e57283863709bb2cb6b5617dfc652722
Victor Gutierrez Diban <vgutierrezdiban@protonmail.com>
2020-11-01 11:50:29 -0300

    Generate model for cities

---
+++ db/migrate/20201101144949_create_cities.rb
@@ -0,0 +1,10 @@
+class CreateCities < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cities do |t|
+      t.string :name, null: false
+      t.string :key, null: false
+
+      t.timestamps
+    end
+  end
+end
